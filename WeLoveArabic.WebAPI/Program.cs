00fca385d1db438073bc808233f6d1c328ad485b
Firas Briki <firas.brikii12@gmail.com>
2026-02-22 18:08:47 +0100

    Add SchemaHashTable & RootAvlTree

--- WeLoveArabic.WebAPI/Program.cs
+++ WeLoveArabic.WebAPI/Program.cs
@@ -1,5 +1,11 @@
+using WeLoveArabic.WebAPI.Services.DataStructures.RootAvlTree;
+using WeLoveArabic.WebAPI.Services.DataStructures.SchemaHashTable;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+
+builder.Services.AddSingleton<SchemaHashTable>();
+builder.Services.AddSingleton<RootAvlTree>();
 
 var app = builder.Build();
 app.UseHttpsRedirection();
--- WeLoveArabic.WebAPI/Services/WeLoveArabicService.cs
+++ WeLoveArabic.WebAPI/Services/WeLoveArabicService.cs
@@ -1,6 +1,19 @@
-﻿namespace WeLoveArabic.WebAPI.Services
+﻿using WeLoveArabic.WebAPI.Services.DataStructures.RootAvlTree;
+using WeLoveArabic.WebAPI.Services.DataStructures.SchemaHashTable;
+
+namespace WeLoveArabic.WebAPI.Services
 {
     public class WeLoveArabicService
     {
+        private readonly SchemaHashTable _schemas;
+        private readonly RootAvlTree _roots;
+
+        public WeLoveArabicService(
+            SchemaHashTable schemaHashTable,
+            RootAvlTree rootAvlTree)
+        {
+            _schemas = schemaHashTable;
+            _roots = rootAvlTree;
+        }
     }
 }
